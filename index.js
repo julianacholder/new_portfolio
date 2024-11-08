0828ce4f82b01601cc3044627a1db9221949936c
Juliana Crystal Holder <114365616+kiiing-crystal@users.noreply.github.com>
2024-11-08 17:44:52 +0200

    finished portfolio

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+function resetForm() {
+  document.querySelector('.input-field').reset();
+}
